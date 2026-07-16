a995ec7c187e3293dfc75c114b39d371f062f53c
kristina378 <kristinadubiaha@gmail.com>
2026-07-16 22:42:34 +0200

    feat: add global admin profile for developers

---
+++ Migrations/20260716204040_AddGlobalAdmin.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quantify.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGlobalAdmin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Discriminator", "Email", "Name", "PasswordHash", "Permission", "PhoneNumber", "Surname" },
+                values: new object[] { 1L, "Admin", "kristinadubiaha07@gmail.com", "admin", "AQAAAAIAAYagAAAAEBqodLtc/3OaaYL66QgSnrVeCERzGJUzP0YuXTCs5JKrgvbXm4bFKp7VQgJeAL1jRA==", 1, "12345678", "anonymous" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1L);
+        }
+    }
+}
